1b4b722d95232f02ec0ff44322f933f98bf1c62b
Senthil Kumar R <Senthil Kumar R>
2017-03-05 01:36:31 -0500

    fifth

---
+++ src/test/java/IT/ITsample2.java
@@ -0,0 +1,16 @@
+package IT;
+
+import org.testng.annotations.Test;
+
+public class ITsample2 {
+	
+	@Test
+	public void method(){
+		
+		System.out.println("_____________222222 Integration Test______________");
+		
+		
+		
+	}
+
+}
